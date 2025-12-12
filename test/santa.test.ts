521cbc14b5ec3a1d4028a3f0c5fd2106f84de8f6
Mykhailo Markhain <mmarkhai@amazon.com>
2025-12-11 20:23:50 -0600

    Initial commit

---
+++ test/santa.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Santa from '../lib/santa-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/santa-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Santa.SantaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
